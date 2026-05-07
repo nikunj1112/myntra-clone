7fa9fe604c3900610b69e948d0154c7e8aa7eccf
RANA NIIKUNJ <8891nikunjrana@gmail.com>
2026-05-08 01:22:25 +0530

    Update recentlyViewedRoutes.js

--- backend/routes/recentlyViewedRoutes.js
+++ backend/routes/recentlyViewedRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const RecentlyViewed = require("../models/RecentlyViewed");
 
+
+
 // ==========================
 // GET Recently Viewed
 // ==========================
